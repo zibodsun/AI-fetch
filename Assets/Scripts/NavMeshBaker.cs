64df83e7a63bb9659c182942f8c6d4925430d50d
FSE_VR23 <FSE_VR23@LAPTOP-OE4HKTPU>
2023-12-06 11:30:18 +0000

    Scene with Dog

---
+++ Assets/Scripts/NavMeshBaker.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using Unity.AI.Navigation;
+using UnityEngine.AI;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    public ARPlaneManager planeManager;
+    public Transform player;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var plane in planeManager.trackables)
+        {
+            if (plane.transform.position.y < player.position.y)
+            {
+                plane.GetComponent<NavMeshSurface>().BuildNavMesh();
+                agent.enabled = true;
+            }
+        }
+    }
+}
